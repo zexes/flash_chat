2908f30f709614219e2975c5ee7021bd237dad07
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-01-10 18:49:11 +0100

    Added to bottom not the top. closes #14

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -138,7 +138,7 @@ class MessagesStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data.documents;
+        final messages = snapshot.data.documents.reversed;
         for (var message in messages) {
           final messageText = message.data['text'];
           final messageSender = message.data['sender'];
