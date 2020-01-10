a4e62889af48f06337384e85cf23874ec1f7fec1
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-01-10 18:07:26 +0100

    overflowing flash icon fixed. closes #11

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -49,11 +49,13 @@ class _LoginScreenState extends State<LoginScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 125.0,
-                  child: Image.asset('images/logo.png'),
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
                 ),
               ),
               SizedBox(
